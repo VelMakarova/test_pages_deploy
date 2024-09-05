abc67461cc309449f82fb137537ffe85aeb9931c
vel-mak <valeria.v.makarova@gmail.com>
2024-09-05 18:21:54 +0300

    false cors

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'https://github.com/VelMakarova/test_pages_deploy.git',
+  server: {
+    cors: false
+  },
   plugins: [react()],
 })
